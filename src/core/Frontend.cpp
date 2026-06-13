9ad0e0a4d6d3a79d8768ba7ec004a0b67064289b
idk1703 <artem.veschitsky@gmail.com>
2026-06-13 20:55:46 +0300

    Disable vertical mouse acceleration adjustment
    Comment out the vertical mouse acceleration adjustment.

--- src/core/Frontend.cpp
+++ src/core/Frontend.cpp
@@ -763,7 +763,7 @@ CMenuManager::CheckSliderMovement(int value)
 		TheCamera.m_fMouseAccelHorzntl += value * 1.0f/200.0f/15.0f;	// probably because diving it to 15 instead of 16(MENUSLIDER_LOGICAL_BARS) had more accurate steps
 		TheCamera.m_fMouseAccelHorzntl = Clamp(TheCamera.m_fMouseAccelHorzntl, 1.0f/3200.0f, 1.0f/200.0f);
 #ifdef FIX_BUGS
-		TheCamera.m_fMouseAccelVertical = TheCamera.m_fMouseAccelHorzntl + 0.0005f;
+		// TheCamera.m_fMouseAccelVertical = TheCamera.m_fMouseAccelHorzntl + 0.0005f;
 #endif
 		break;
 #ifdef CUSTOM_FRONTEND_OPTIONS
@@ -3172,7 +3172,7 @@ CMenuManager::LoadSettings()
 #endif
 
 #ifdef FIX_BUGS
-	TheCamera.m_fMouseAccelVertical = TheCamera.m_fMouseAccelHorzntl + 0.0005f;
+	// TheCamera.m_fMouseAccelVertical = TheCamera.m_fMouseAccelHorzntl + 0.0005f;
 #endif
 #ifdef PC_PLAYER_CONTROLS
 	CCamera::m_bUseMouse3rdPerson = m_ControlMethod == CONTROL_STANDARD;
